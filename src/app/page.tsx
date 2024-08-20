d6ad34b43b797657c38a15da0edc9d98301ccef6
Martin Macharia <mosmartin5@gmail.com>
2024-08-21 00:04:18 +0200

    feat: add the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <main className="flex h-screen max-h-screen"></main>;
+  return (
+    <main className="flex h-screen max-h-screen">
+      {/* TODO: otp verification | passkey modal */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient logo"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <footer className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 St. Martin Healthcare. All rights reserved.
+            </p>
+
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </footer>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
+    </main>
+  );
 }
