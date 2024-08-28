b48b47d01e7e0417ab788a25ce5edbea06b53dd7
Martin Macharia <mosmartin5@gmail.com>
2024-08-28 22:27:43 +0200

    feat: add an initial form

--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -1,8 +1,19 @@
 "use client";
 
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -22,5 +33,40 @@ export default function PatientForm() {
     console.log(values);
   }
 
-  return <div>Patient Form</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 🫶</h1>
+
+          <p className="text-dark-700">
+            Welcome to St. Martin Healthcare. We are excited to have you here.
+            Please fill in the form below to get started.
+          </p>
+        </section>
+
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+
+              <FormControl>
+                <Input placeholder="martin" {...field} />
+              </FormControl>
+
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
